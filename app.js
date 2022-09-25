dd9d76d5893b2ffec8695cc8d7c085f40852a368
LewBlu <lewis.blundell.development@gmail.com>
2022-09-25 15:40:44 +0100

    added https

--- app.js
+++ app.js
@@ -6,16 +6,10 @@ const session = require('express-session');
 const passport = require('passport');
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const https = require('https')
-const fs = require('fs')
-
-
-
-
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
-
-
 
 // Create connection
 const db = mysql.createConnection({
@@ -40,27 +34,28 @@ require('dotenv').config()
 
 
 
-// Use Cors
-app.use(cors({
-    origin:"http://127.0.0.1:5500",
-    credentials: true
-}))
-
-// app.set('trust proxy', 1);
-
+// Set CORS options to allow access via specific hosts and manage type of requests
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'CHANGE THIS TO FRONTEND URL'); // * allows all, or you can limit by domain
+    res.setHeader('Access-Control-Allow-Methods', '*'); // Set which header methods you want to allow (GET,POST,PUT,DELETE,OPTIONS)
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // These 2 are recommended
+    res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie'); // Required to allow the returned cookie to be set
+	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Required to allow auth credentials
+    next();
+});
 
 // Express Session
 app.use(session({
     secret: 'secert',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { 
-        httpOnly: true,
-        path:'/',
-        secure: true,
-        sameSite: "none",
-        maxAge:3600000
-    }
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+		path: "/",
+		httpOnly: false, // Set this so it can be accessed via document.cookie in javascript
+		secure: true, // Required when using sameSite:'none'
+		sameSite: 'none', // Set this to allow access via different domians
+		maxAge: 3600000 // Set cookie to last 1 hour
+	}
   }))
 
 
@@ -68,12 +63,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
 app.use(express.json());
-//app.use(express.urlencoded({extended: false}));
-app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Connect flash
 app.use(flash());
@@ -95,13 +87,9 @@ app.use('/api/user', require('./routes/subgoals'));
 
 const PORT = process.env.PORT || 3000;
 
+// Sets up the server as HTTPS using certificates
 const httpsOptions = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-}    
-const server = https.createServer(httpsOptions, app).listen(3000)
-
-
-// app.listen(PORT, () => {
-//     console.log(`server start on ${PORT}`)
-// })
+	key: fs.readFileSync('key.pem'),
+	cert: fs.readFileSync('cert.pem')
+}
+const server = https.createServer(httpsOptions, app).listen(PORT);
