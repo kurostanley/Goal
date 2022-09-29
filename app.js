6986d404626b688a6b41d34c7e6a39018e3595ad
Shin Ting Lin <kurostanley@gmail.com>
2022-09-29 18:16:31 +0800

    to the state before ssl

--- app.js
+++ app.js
@@ -34,23 +34,23 @@ require('dotenv').config()
 
 
 
-// // Use Cors
-// app.use(cors({
-//     origin:"https://localhost:5000",
-//     credentials: true
-// }))
+// Use Cors
+app.use(cors({
+    origin:"https://localhost:5000",
+    credentials: true
+}))
 
 // app.set('trust proxy', 1);
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://localhost:5000'); // * allows all, or you can limit by domain
-    res.setHeader('Access-Control-Allow-Methods', '*'); // Set which header methods you want to allow (GET,POST,PUT,DELETE,OPTIONS)
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // These 2 are recommended
-    res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie'); // Required to allow the returned cookie to be set
-	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Required to allow auth credentials
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://localhost:5000'); // * allows all, or you can limit by domain
+//     res.setHeader('Access-Control-Allow-Methods', '*'); // Set which header methods you want to allow (GET,POST,PUT,DELETE,OPTIONS)
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // These 2 are recommended
+//     res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie'); // Required to allow the returned cookie to be set
+// 	res.setHeader('Access-Control-Allow-Credentials', 'true'); // Required to allow auth credentials
+//     next();
+// });
 
 // Express Session
 app.use(session({
@@ -99,10 +99,12 @@ app.use('/api/user', require('./routes/subgoals'));
 
 const PORT = process.env.PORT || 3000;
 
-// Sets up the server as HTTPS using certificates
-const httpsOptions = {
-	key: fs.readFileSync('key.pem'),
-	cert: fs.readFileSync('cert.pem')
-}
-const server = https.createServer(httpsOptions, app).listen(PORT);
+// // Sets up the server as HTTPS using certificates
+// const httpsOptions = {
+// 	key: fs.readFileSync('key.pem'),
+// 	cert: fs.readFileSync('cert.pem')
+// }
+// const server = https.createServer(httpsOptions, app).listen(PORT);
+
+app.listen(PORT)
 
